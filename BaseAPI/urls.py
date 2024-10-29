cf3a331b950a1c343f60d18c4d8d3a49a04b042e
Stefcool8 <stefan.nastasiu8@gmail.com>
2024-10-29 17:38:45 +0200

    stef-dev: Refactoring in project structure. Separation into general and ml subfolders.

--- BaseAPI/urls.py
+++ BaseAPI/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from api.controllers.test_controller import TestController
+from api.controllers.general.test_controller import TestController
 
 urlpatterns = [
     path('admin/', admin.site.urls),
