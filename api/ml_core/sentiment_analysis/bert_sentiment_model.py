b22833355cc0cce82cf49d1c2949adc42d8a4dc9
Stefcool8 <stefan.nastasiu8@gmail.com>
2024-12-15 17:44:36 +0200

    stef-dev: Changed the return value of Bert Sentiment Model to represent a continuous value between 0 and 2 (instead of a percentage). This should help with the node coloring.

--- api/ml_core/sentiment_analysis/bert_sentiment_model.py
+++ api/ml_core/sentiment_analysis/bert_sentiment_model.py
@@ -46,12 +46,18 @@ class BertSentimentModel(SentimentModelInterface):
         output = self.model(**encoded_input)
         scores = output[0][0].detach().numpy()
         scores = softmax(scores)
+
+        # Compute continuous sentiment score
+        sentiment_score = np.dot(scores, [0, 1, 2])
+
         ranking = np.argsort(scores)
         ranking = ranking[::-1]
 
         predicted_label = ranking[0]
         predicted_score = np.round(float(scores[predicted_label]), 4)
         print(f"Predicted: {self.config.id2label[predicted_label]}, "
-              f"Value: {predicted_score}\n")
+              f"Class Percentage: {predicted_score}\n",
+              f"Continuous Score: {sentiment_score:.2f} (0=negative, 1=neutral, 2=positive)")
 
-        return predicted_score
+        # Return the continuous sentiment score with 4 decimal places
+        return round(sentiment_score, 4)
