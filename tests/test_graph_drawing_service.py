d723e8bdac4f07501c26f04839ef0df37b9a5e6f
SamuelLazurca <samilazurca15@gmail.com>
2024-12-17 20:31:49 +0200

    deleted files

--- tests/test_graph_drawing_service.py
+++ tests/test_graph_drawing_service.py
@@ -4,8 +4,6 @@ from api.services.general.graph_drawing_service import GraphDrawingService
 from api.services.layouts.Layout import Layout
 
 from django.test import TestCase
-
-from api.services.layouts.simple_circular_drawing import SimpleCircularDrawing
 import os
 
 
@@ -13,8 +11,3 @@ class GraphDrawingServiceTests(TestCase):
     def setUp(self):
         self.graph_drawing_service = GraphDrawingService()
         self.graph = nx.read_graphml("./resources/graphs/example.graphml")
-
-    def test_graph_drawing_service(self):
-        layout = SimpleCircularDrawing()
-        graph_drawing = self.graph_drawing_service.generate_drawing(layout, self.graph)
-        self.assertNotEqual(graph_drawing, False)
