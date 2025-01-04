de7cfd14c2027fd7c48a3bcce6ae435c9b536493
florin rotaru <florineugen271@gmail.com>
2025-01-04 17:11:41 +0200

    added clear comments graph/drawing method, added post service class, refactored posts repo a bit

---
+++ api/services/general/posts_service.py
@@ -0,0 +1,12 @@
+from bson import ObjectId
+from BaseAPI.settings import BASE_DIR
+
+from api.repositories.general.posts_repository import PostsRepository
+
+class PostsService:
+
+    def get_topic_posts(self, topic):
+        with PostsRepository() as pr:
+            return pr.get_by_topic(topic)
+
+res = PostsService().get_topic_posts('politics')
